db422850654af441d4a15f5b1a2a9c5c240976d0
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2018-10-05 09:59:57 +0900

    Initial Commit

---
+++ SourceControlTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SourceControlTest
+//
+//  Created by 上田和章 on 2018/10/05.
+//  Copyright © 2018 netplan matsuyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
